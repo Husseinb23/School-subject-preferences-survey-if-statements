7289ad041e81859631800c71629f276cb32b7447
Hussein Barakat <121978208+Husseinb23@users.noreply.github.com>
2023-02-11 22:31:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#=== School subject prefrences===
+englishOrmath = input("Do you prefer English or Math?: ")
+if englishOrmath == "math":
+ print("Numbers guy eh")
+else:
+ print("essay kid")
+TechOrBusiness = input("Are more intrested in tech or business: ")
+if TechOrBusiness == "tech":
+  print("Cool! your a techy")
+else:
+  print("Business person I see")
+SceinceOrmath = input("Do your prefer Math or Sceince?: ")
+if SceinceOrmath == "sceince":
+  print("Sceince nerd!")
+else:
+  print("you are a genius!")
+
+
+
+
